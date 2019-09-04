4bba029625b76e9d77d0e8bba7c16ed40a115d43
Jason <37201661+Jason171096@users.noreply.github.com>
2019-09-04 13:59:02 -0500

    Create problem_1494.cs

---
+++ 1494 - Hey don't Read!!!/problem_1494.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace problem_1494
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Console.WriteLine("I got my first solution!!!");
+        }
+    }
+}
