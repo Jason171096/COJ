bcdc4fb1134978e48d88949522f05f55a177b908
Jason <37201661+Jason171096@users.noreply.github.com>
2019-09-04 13:54:10 -0500

    Create problem_1494.java

---
+++ 1494 - Hey don't Read!!!/problem_1494.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+
+public class problem_1494 {
+    public static void main(String[] args) {
+        Scanner e = new Scanner(System.in);
+    String s = e.nextLine(); 
+    System.out.println("I got my first solution!!!");
+    }   
+}
