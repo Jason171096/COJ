bbc31a96b50393a85affeedc1718134ae5bc17b3
Jason <37201661+Jason171096@users.noreply.github.com>
2019-09-04 23:09:22 -0500

    Create problem_2185.java

---
+++ 2185 - A ? B/problem_2185.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class problem_2185 {
+    public static void main(String[] args) {
+      Scanner e = new Scanner(System.in);
+    int A = e.nextInt();
+    int B = e.nextInt();
+    int suma,resta,multi,divi,mod;
+    suma=A+B;
+    resta=A-B;
+    multi=A*B;
+    divi=A/B;
+    mod=A%=B;
+        System.out.println(suma);
+        System.out.println(resta);
+        System.out.println(multi);
+        System.out.println(divi);
+        System.out.println(mod);
+    }
+    
+    
+}
