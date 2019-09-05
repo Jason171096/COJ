61b0011539d342a6cafbe85faec51540a2d98830
Jason <37201661+Jason171096@users.noreply.github.com>
2019-09-05 16:05:29 -0500

    Create problem_2620.java

---
+++ 2620 - Aaaaaaaah/problem_2620.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class problem_2620 {
+    public static void main(String[] args) {
+        Scanner e = new Scanner(System.in);
+        int r = e.nextInt();
+        int d = 2*r;
+        System.out.print("A");
+        for(int i=0; i<(2*d); i++)
+        {
+            System.out.print("a");
+        }
+        System.out.println("h");
+    }
+}
