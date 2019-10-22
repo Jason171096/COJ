d2b459d01a1f53928765bfe6bc64ccf1f9583fff
Jason <37201661+Jason171096@users.noreply.github.com>
2019-10-21 19:58:42 -0500

    Create problem_3475.java

---
+++ 3475 - Problema AB/problem_3475.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+
+public class problem_3475 {
+    public static void main(String[] args) {
+        Scanner e = new Scanner(System.in);
+
+        long A = e.nextLong();
+        long B = e.nextLong();
+        System.out.println(A*B);
+    }
+    
+}
