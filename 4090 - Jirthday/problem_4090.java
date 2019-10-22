fb73156a9055fd83f073cd510cc82ca9eeb7795c
Jason <37201661+Jason171096@users.noreply.github.com>
2019-10-21 19:57:22 -0500

    Create problem_4090.java

---
+++ 4090 - Jirthday/problem_4090.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+
+public class problem_4090 {
+    public static void main(String[] args) {
+        Scanner e = new Scanner(System.in);
+        int futuro = 3000;
+        int cumple = e.nextInt();
+        System.out.println(futuro-cumple);
+    }
+}
