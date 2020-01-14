d5b06c5d5e209bbbd33600b4f164eb45b9167c80
Jason <37201661+Jason171096@users.noreply.github.com>
2020-01-14 13:35:22 -0600

    Create problem_2769.java

---
+++ 2769 - Día del Ingeniero/problem_2769.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class problem_2769 {
+    public static void main(String[] args) {
+        Scanner e = new Scanner(System.in);
+        int N = e.nextInt();
+        for (int i = 1; i <= N; i++) {
+            System.out.println(i+": I am participating in the Engineer's day.");
+        }
+    }
+}
