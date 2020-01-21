cf9b2e8ff8c621b6276b72e867b2d3a8b7305852
Jason <37201661+Jason171096@users.noreply.github.com>
2020-01-20 22:56:21 -0600

    Create problem_4092.cpp

---
+++ 4092 - L - Ligero/problem_4092.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int B;
+	cin>>B;
+	if(B<2)
+		cout<<"WRONG ANSWER";
+	else if(B>3)
+		cout<<"RUNTIME ERROR";
+	else
+		cout<<"ACCEPTED";
+	return 0;
+}
