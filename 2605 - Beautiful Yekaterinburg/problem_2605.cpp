51a80dd244da84633cabc73bab46350a937bab00
Jason <37201661+Jason171096@users.noreply.github.com>
2020-02-25 15:42:32 -0600

    Create problem_2605.cpp

---
+++ 2605 - Beautiful Yekaterinburg/problem_2605.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+	int D;
+	cin >> D;
+	switch(D)
+	{
+		case 1: 
+			cout << "1" << endl;
+			break;
+		case 2: 
+			cout << "7" << endl;
+			break;
+		case 3:
+			cout << "2" << endl;
+			break;
+		case 4:
+			cout << "3" << endl;
+			break;
+	}
+	return 0;
+}
